f0b66eb2cfd9d3281496dad2ad7e08e951966012
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2021-05-21 13:18:26 -0300

    Testing Method: Specific and General ( String Test)

---
+++ TestNinja.UnitTests/Fundamentals/HtmlFormatterTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests.Fundamentals
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenIsCalled_ReturnBoldString()
+        {
+            //Arrange
+            var fmt = new HtmlFormatter();
+            //Act
+            var result = fmt.FormatAsBold("abc");
+            //Assert
+
+            //Specific
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>").IgnoreCase);
+
+            //More General
+            Assert.That(result, Does.StartWith("<strong>").IgnoreCase);
+            Assert.That(result, Does.EndWith("</strong>").IgnoreCase);
+            Assert.That(result, Does.Contain("abc").IgnoreCase);
+        }
+    }
+}
