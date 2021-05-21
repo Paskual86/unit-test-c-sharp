fde03f54ebfbc02bf6cf9f7bad6444024b4a14c5
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2021-05-21 15:09:40 -0300

    Testing void methods

---
+++ TestNinja.UnitTests/Fundamentals/ErrorLoggerTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests.Fundamentals
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenIsCalled_ResultsLastErrorCode()
+        {
+            var log = new ErrorLogger();
+            log.Log("error");
+            Assert.That(log.LastError, Is.EqualTo("error"));
+        }
+    }
+}
