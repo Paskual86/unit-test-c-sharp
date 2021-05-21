3661ada3ceb9e286ac3e1f1dbbc7c883af1be76c
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2021-05-21 13:39:57 -0300

    Unit Test : Arrays and collections

--- TestNinja.UnitTests/Fundamentals/MathTests.cs
+++ TestNinja.UnitTests/Fundamentals/MathTests.cs
@@ -6,15 +6,23 @@ namespace TestNinja.UnitTests.Fundamentals
     [TestFixture]
     public class MathTests
     {
+        private Math _math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
         [Test]
         [TestCase(1,2,3)]
         [Ignore("Porque si!")]
         public void Add_WhenExecuted_ResultSumValues(int a, int b, int expectedResult) 
         {
             //Arrange
-            var math = new Math();
+            //var math = new Math();
             //Act
-            var result = math.Add(a, b);
+            var result = _math.Add(a, b);
             //Assert
             Assert.That(result, Is.EqualTo(expectedResult));
         }
@@ -26,11 +34,33 @@ namespace TestNinja.UnitTests.Fundamentals
         public void Max_WhenExecuted_ResultTheGreaterArgument(int a, int b, int expectedResult)
         {
             // Arrange
-            var math = new Math();
+            //var math = new Math();
             // Act
-            var result = math.Max(a, b);
+            var result = _math.Max(a, b);
             // Assert
             Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+
+        [Test]
+        public void GetOddNumbers_LimitIsGreaterThanZero_ReturnOddNumbersUpToLimits()
+        {
+            // Devuelve un array
+            var result = _math.GetOddNumbers(5);
+
+            // Chequeamos que no este vacio
+            // Assert.That(result, Is.Not.Empty);
+            // O chequeamos que la cantidad devuelta sea 3
+            // Assert.That(result.Count(), Is.EqualTo(3));
+            // O chequeamos los valores
+            /*Assert.That(result, Does.Contain(1));
+            Assert.That(result, Does.Contain(3));
+            Assert.That(result, Does.Contain(5));*/
+
+            Assert.That(result, Is.EquivalentTo(new[] {1, 3, 5}));
+
+            // Assert.That(result, Is.Ordered);
+            // Assert.That(result, Is.Unique);
         }
     }
 }
