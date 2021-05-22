bcbd5b3e297db25290fab52664f49a13893ded7e
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2021-05-22 05:48:17 -0300

    Exercise: FizzBuzz algorithm

---
+++ TestNinja.UnitTests/Fundamentals/FizzBuzzTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests.Fundamentals
+{
+    public class FizzBuzzTests
+    {
+        [Test]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(9, "Fizz")]
+        [TestCase(25, "Buzz")]
+        [TestCase(4, "4")]
+        public void GetOutput_WhenIsCalled_ResultsTheExpectedValues(int value, string expectedResult)
+        {
+            var result = FizzBuzz.GetOutput(value);
+            Assert.That(result, Is.EqualTo(expectedResult).IgnoreCase);
+        }
+    }
+}
