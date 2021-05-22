5bb610c45ad7ed81fab3bccffd612338fd2bb0b7
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2021-05-22 05:32:54 -0300

    Validate Events

--- TestNinja.UnitTests/Fundamentals/ErrorLoggerTests.cs
+++ TestNinja.UnitTests/Fundamentals/ErrorLoggerTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests.Fundamentals
@@ -24,6 +25,16 @@ namespace TestNinja.UnitTests.Fundamentals
             Assert.That(() => log.Log(error), Throws.ArgumentNullException);
             // Otra forma para excepciones mas generales
             //Assert.That(() => log.Log(error), Throws.Exception.TypeOf<DivideByZeroException>);
+        }
+
+        [Test]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var log = new ErrorLogger();
+            var id = Guid.Empty;
+            log.ErrorLogged += (sender, args) => { id = args; };
+            log.Log("a");
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
         }
     }
 }
