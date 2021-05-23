d2c4ad77926047115687adbcd60c1a4a2a41ed16
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2021-05-23 10:25:03 -0300

    Refactoring the test source code

---
+++ TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class InstallerHelperTests
+    {
+        //private Mock<>
+        
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+    }
+}
