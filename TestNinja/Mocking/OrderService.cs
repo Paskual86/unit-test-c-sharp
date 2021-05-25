f6d76c889741c04d875b6d40a9cddc7588c4840a
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2021-05-25 02:41:42 -0300

    unit test

--- TestNinja/Mocking/OrderService.cs
+++ TestNinja/Mocking/OrderService.cs
@@ -12,9 +12,7 @@
         public int PlaceOrder(Order order)
         {
             var orderId = _storage.Store(order);
-            
             // Some other work
-
             return orderId; 
         }
     }
